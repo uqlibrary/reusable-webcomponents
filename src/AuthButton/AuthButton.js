f6722e4b7e98625c73d79a4516003125c440657f
Lea de Groot <l.degroot@uq.edu.au>
2021-03-12 08:30:30 +1000

    make the auth button line up with askus and mylibrary [PT #177107792]

--- src/AuthButton/AuthButton.js
+++ src/AuthButton/AuthButton.js
@@ -18,7 +18,7 @@ unauthorisedtemplate.innerHTML = `
                 <svg class="MuiSvgIcon-root-167 makeStyles-iconButton-180" focusable="false" viewBox="0 0 24 24" aria-hidden="true" id="logged-out-icon">
                     <path d="M12 5.9c1.16 0 2.1.94 2.1 2.1s-.94 2.1-2.1 2.1S9.9 9.16 9.9 8s.94-2.1 2.1-2.1m0 9c2.97 0 6.1 1.46 6.1 2.1v1.1H5.9V17c0-.64 3.13-2.1 6.1-2.1M12 4C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z"></path>
                 </svg>
-                <div id="log-in-label">Log in</div>
+                <div id="log-in-label" class="button-label">Log in</div>
             </span>
             <span class="MuiTouchRipple-root-412"></span>
         </button>
@@ -40,7 +40,7 @@ authorisedtemplate.innerHTML = `
                 <svg class="MuiSvgIcon-root-167 makeStyles-iconButton-180" focusable="false" viewBox="0 0 24 24" aria-hidden="true" id="logged-in-icon">
                     <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path>
                 </svg>
-                <div id="log-out-label">Log out</div>
+                <div id="log-out-label" class="button-label">Log out</div>
             </span>
             <span class="MuiTouchRipple-root-412"></span>
         </button>
