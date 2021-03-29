258d4305b7cc7b31613fda75efab799ccd262a8f
Ky Lane <k.lane@uq.edu.au>
2021-03-29 13:35:53 +1000

    Small fix

--- src/Alert/Alert.js
+++ src/Alert/Alert.js
@@ -30,7 +30,7 @@ class Alert extends HTMLElement {
 
         const icons = {
             0: '<svg viewBox="0 0 24 24" aria-hidden="false" id="info-outline-icon" aria-label="Alert."><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path></svg>',
-            1: '<svg class="MuiSvgIcon-root icon" focusable="true" viewBox="0 0 24 24" aria-hidden="false" id="warning-icon" aria-label="Important alert."><path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"></path></svg>',
+            1: '<svg viewBox="0 0 24 24" aria-hidden="false" id="warning-icon" aria-label="Important alert."><path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"></path></svg>',
         }
 
         // Set the title and message
