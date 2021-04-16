f9136ab32cf131ddd99e8f596e735ab840e1d0e0
Lea de Groot <l.degroot@uq.edu.au>
2021-04-16 11:57:42 +1000

    insert the new header items [PT #177032816]

--- src/applications/studenthub/load.js
+++ src/applications/studenthub/load.js
@@ -1,99 +1,151 @@
-var urlStudentHubHomePage = "https://"+window.location.hostname+"/workgroups/library-staff-development";
+const studentHubHomePageUrl = "https://" + window.location.hostname + "/workgroups/library-staff-development";
 // note: function isHomePage also hard codes this path
 
 function ready(fn) {
-  if (document.readyState !== 'loading'){
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
+    if (document.readyState !== 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+function createSlotForButtonInUtilityArea(button, id = null) {
+    const slot = document.createElement('span');
+    !!slot && slot.setAttribute('slot', 'site-utilities');
+    !!slot && !!id && slot.setAttribute('id', id);
+    !!button && !!slot && slot.appendChild(button);
+    return slot;
+}
+
+function createAskusButton() {
+    if (!!document.querySelector('askus-button')) {
+        return false;
+    }
+
+    const askusButton = document.createElement('askus-button');
+    const slot = !!askusButton && createSlotForButtonInUtilityArea(askusButton, 'askus');
+
+    return slot;
 }
 
 function loadReusableComponents() {
-  // loadUQFavicon();
-  // addAppleTouchIcon();
+    loadUQFavicon();
+    // addAppleTouchIcon();
 
-  addBreadcrumbs('#head');
+    addBreadcrumbs('#head');
 
-  updateEventsLinkText();
+    updateEventsLinkText();
 
-  reformatSidebarDates();
+    reformatSidebarDates();
 
-  //insert elements, even before Polymer is loaded
+    //first element of the original document
+    const firstElement = document.body.children[0];
+    if (!firstElement) {
+        return;
+    }
 
-  //first element of the original document
-  var firstElement = document.body.children[0];
+    if (!document.querySelector('uq-gtm')) {
+        const gtm = document.createElement('uq-gtm');
+        !!gtm && document.body.insertBefore(gtm, firstElement);
+    }
 
-  //insert alerts before header
-  var alerts = document.querySelector('uqlibrary-alerts');
-  if (!alerts) {
-    //as a back up insert header if it's not defined already
-    alerts = document.createElement('uqlibrary-alerts');
-    document.body.insertBefore(alerts, firstElement);
-  }
+    if (!document.querySelector('uq-header')) {
+        const header = document.createElement('uq-header');
+        header.setAttribute("hideLibraryMenuItem", "");
+        header.setAttribute("skipnavid", "sitenameanchor");
+        !!header && document.body.insertBefore(header, firstElement);
+    }
 
-  // insert header after body-tag
-  var header = document.createElement('uq-minimal-header');
-  document.body.insertBefore(header, firstElement);
+    if (!document.querySelector('uq-site-header')) {
+        const siteHeader = document.createElement('uq-site-header');
 
-  // insert sub footer before body-tag
-  var subFooter = document.createElement('uql-connect-footer');
-  document.body.appendChild(subFooter);
+        const askusButton = createAskusButton();
+        !!askusButton && siteHeader.appendChild(askusButton);
 
-  // insert footer before body-tag
-  var footer = document.createElement('uq-minimal-footer');
-  document.body.appendChild(footer);
+        !!siteHeader && document.body.insertBefore(siteHeader, firstElement);
+    }
 
+    if (!document.querySelector('alert-list')) {
+        const alerts = document.createElement('alert-list');
+        !!alerts && document.body.insertBefore(alerts, firstElement);
+    }
 
-  window.addEventListener('WebComponentsReady', function() {
-    // when polymer is ready - configure elements
-    header.showLoginButton = false;
-  });
+    if (!document.querySelector('connect-footer')) {
+        const connectFooter = document.createElement('connect-footer');
+        document.body.appendChild(connectFooter);
+    }
 
+    if (!document.querySelector('uq-footer')) {
+        const subFooter = document.createElement('uq-footer');
+        !!subFooter && document.body.appendChild(subFooter);
+    }
+}
+
+function addSkipNavLandingPoint() {
+    const sitenameAnchor = document.querySelector('#sitename a');
+    if (!sitenameAnchor) {
+        // return;
+    }
+    sitenameAnchor.id = 'sitenameanchor';
+}
+
+function loadUQFavicon() {
+    const link = document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = '//www.library.uq.edu.au/favicon.ico';
+
+    document.getElementsByTagName('head')[0].appendChild(link);
+
+    //for IE
+    link.rel = 'icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
 }
 
 /**
  * Reformat date from DD-MMM-YYYY to styled elements
  */
 function reformatSidebarDates() {
-  const upcomingEvents = document.querySelectorAll('.upcomingEvents .body li');
+    const upcomingEvents = document.querySelectorAll('.upcomingEvents .body li');
 
-  if (!upcomingEvents.length) {
-    return;
-  }
-
-  upcomingEvents.forEach(event => {
-
-    const originalDate = event.querySelector('span.caption').innerHTML.replace(/(\s|\n)+/g, '-');
-    if (originalDate) {
-
-      const dateBits = originalDate.split("-");
-      if (dateBits.length > 2) {
-
-        //hide original date display
-        event.querySelector('span.caption').className += ' hide';
-
-        //create day element
-        const dayElement = document.createElement('div');
-        dayElement.className = 'day';
-        dayElement.appendChild(document.createTextNode(dateBits[0]));
-
-        //create month element
-        const monthElement = document.createElement('div');
-        monthElement.className = 'month';
-        monthElement.appendChild(document.createTextNode(dateBits[1]));
-
-        //add to event list item
-        dateElement = document.createElement('div');
-        dateElement.className = 'formatted-date';
-        dateElement.appendChild(dayElement);
-        dateElement.appendChild(monthElement);
-
-        const eventLink = event.querySelector('a');
-        event.insertBefore(dateElement, eventLink);
-      }
+    if (!upcomingEvents.length) {
+        return;
     }
-  });
+
+    upcomingEvents.forEach(event => {
+
+        const originalDate = event.querySelector('span.caption')
+            .innerHTML
+            .replace(/(\s|\n)+/g, '-'); // this replace may not be needed now - they used to have some junk char in there
+        if (originalDate) {
+
+            const dateBits = originalDate.split("-");
+            if (dateBits.length > 2) {
+
+                //hide original date display
+                event.querySelector('span.caption').className += ' hide';
+
+                //create day element
+                const dayElement = document.createElement('div');
+                dayElement.className = 'day';
+                dayElement.appendChild(document.createTextNode(dateBits[0]));
+
+                //create month element
+                const monthElement = document.createElement('div');
+                monthElement.className = 'month';
+                monthElement.appendChild(document.createTextNode(dateBits[1]));
+
+                //add to event list item
+                dateElement = document.createElement('div');
+                dateElement.className = 'formatted-date';
+                dateElement.appendChild(dayElement);
+                dateElement.appendChild(monthElement);
+
+                const eventLink = event.querySelector('a');
+                event.insertBefore(dateElement, eventLink);
+            }
+        }
+    });
 }
 
 /**
@@ -101,8 +153,8 @@ function reformatSidebarDates() {
  * @returns {boolean}
  */
 function isHomePage() {
-  var regexp = /https?:\/\/((www\.)?(careerhub|studenthub)\.uq\.edu\.au)\/workgroups\/library-staff-development\/?$/;
-  return regexp.test(window.location.href);
+    const regexp = /https?:\/\/((www\.)?(careerhub|studenthub)\.uq\.edu\.au)\/workgroups\/library-staff-development\/?$/;
+    return regexp.test(window.location.href);
 }
 
 /**
@@ -113,132 +165,132 @@ function isHomePage() {
  * @returns {boolean}
  */
 function addBreadcrumbs(parentElementIdentifier) {
-  var parentBlock = document.querySelector(parentElementIdentifier);
-  if (parentBlock === null) {
-    return false;
-  }
-
-  // create ol
-  var breadcrumbList = document.createElement('ol');
-  breadcrumbList.className = 'breadcrumbList';
-
-  // create first breadcrumb entry: home page
-  // <paper-icon-button icon="home"></paper-icon-button>
-  var homepageIcon = document.createElement('paper-icon-button');
-  homepageIcon.icon= 'home';
-  var homePageLink = 'https://www.library.uq.edu.au/';
-
-  var anAnchor = document.createElement('a');
-  anAnchor.href = homePageLink;
-  anAnchor.appendChild(homepageIcon);
-
-  var anLI = document.createElement('li');
-  anLI.appendChild(anAnchor);
-  breadcrumbList.appendChild(anLI);
-
-
-  // create second breadcrumb entry: Studenthub workgroup homepage
-  var linktext1 = 'Library ';
-  var linktext2 = 'staff development';
-
-  var childElement;
-  var displayNode;
-
-  anLI = document.createElement('li');
-  anLI.className = 'staffdevhomepage';
-
-  if (isHomePage()) {
-    // spans required for css
-    childElement = document.createElement('span');
-  } else {
-    childElement = document.createElement('a');
-    childElement.href = urlStudentHubHomePage;
-  }
-  var displayNode1 = document.createTextNode(linktext1);
-  var childElement1 = document.createElement('span');
-  childElement1.appendChild(displayNode1);
-  childElement.appendChild(childElement1);
-
-  displayNode = document.createTextNode(linktext2);
-  childElement.appendChild(displayNode);
-
-  anLI.appendChild(childElement);
-  breadcrumbList.appendChild(anLI);
-
-
-  // On the Studenthub event page, event titles have a class of 'event_title'
-  var testElement = document.querySelector('.event_title');
-
-  // third breadcrumb
-  var theLabel = 'Event list';
-  displayNode = document.createTextNode(theLabel);
-  if (testElement !== null) {
-    // we are on an event page - make this a link
-    childElement = document.createElement('a');
-    childElement.href = urlStudentHubHomePage + '/events';
-    childElement.appendChild(displayNode);
-
-    anLI = document.createElement('li');
-    anLI.appendChild(childElement);
-    breadcrumbList.appendChild(anLI);
-
-  } else {
-    if (!isHomePage()) {
-      childElement = document.createElement('span');
-      childElement.appendChild(displayNode);
-
-      anLI = document.createElement('li');
-      anLI.appendChild(childElement);
-      breadcrumbList.appendChild(anLI);
-
-      // add class to body so we know its the list page
-      newclassName = ' listpage';
-      document.body.className+= newclassName;
+    const parentBlock = document.querySelector(parentElementIdentifier);
+    if (parentBlock === null) {
+        return false;
     }
-  }
 
+    // create ol
+    const breadcrumbList = document.createElement('ol');
+    breadcrumbList.className = 'breadcrumbList';
 
-  // fourth breadcrumb
-  if (testElement !== null) {
-    // an event class means we are on a detail page
+    // create first breadcrumb entry: home page
+    // <paper-icon-button icon="home"></paper-icon-button>
+    const homepageIcon = document.createElement('paper-icon-button');
+    homepageIcon.icon = 'home';
+    const homePageLink = 'https://www.library.uq.edu.au/';
 
-    // for desktop, display the event's title as an unlinked breadcrumb
-    // for mobile, display 'event details' - some of the titles are long
-    anLI = document.createElement('li');
+    const anAnchor = document.createElement('a');
+    anAnchor.href = homePageLink;
+    anAnchor.appendChild(homepageIcon);
 
-    var mobileLabel = 'Event details';
-    displayNode = document.createTextNode(mobileLabel);
-    childElement = document.createElement('span');
-    childElement.className = 'mobileOnly';
-    childElement.appendChild(displayNode);
-    anLI.appendChild(childElement);
-
-    var textProperty = 'textContent' in document ? 'textContent' : 'innerText';
-    var nonMobileLabel = testElement[textProperty];
-    displayNode = document.createTextNode(nonMobileLabel);
-    childElement = document.createElement('span');
-    childElement.className = 'nonMobile';
-    childElement.appendChild(displayNode);
-    anLI.appendChild(childElement);
-
+    let anLI = document.createElement('li');
+    anLI.appendChild(anAnchor);
     breadcrumbList.appendChild(anLI);
-  }
 
-  parentBlock.insertBefore(breadcrumbList, parentBlock.firstChild);
 
-  return true;
+    // create second breadcrumb entry: Studenthub workgroup homepage
+    const linktext1 = 'Library ';
+    const linktext2 = 'staff development';
+
+    let childElement;
+    let displayNode;
+
+    anLI = document.createElement('li');
+    anLI.className = 'staffdevhomepage';
+
+    if (isHomePage()) {
+        // spans required for css
+        childElement = document.createElement('span');
+    } else {
+        childElement = document.createElement('a');
+        childElement.href = studentHubHomePageUrl;
+    }
+    const displayNode1 = document.createTextNode(linktext1);
+    const childElement1 = document.createElement('span');
+    childElement1.appendChild(displayNode1);
+    childElement.appendChild(childElement1);
+
+    displayNode = document.createTextNode(linktext2);
+    childElement.appendChild(displayNode);
+
+    anLI.appendChild(childElement);
+    breadcrumbList.appendChild(anLI);
+
+
+    // On the Studenthub event page, event titles have a class of 'event_title'
+    const testElement = document.querySelector('.event_title');
+
+    // third breadcrumb
+    const theLabel = 'Event list';
+    displayNode = document.createTextNode(theLabel);
+    if (testElement !== null) {
+        // we are on an event page - make this a link
+        childElement = document.createElement('a');
+        childElement.href = studentHubHomePageUrl + '/events';
+        childElement.appendChild(displayNode);
+
+        anLI = document.createElement('li');
+        anLI.appendChild(childElement);
+        breadcrumbList.appendChild(anLI);
+
+    } else {
+        if (!isHomePage()) {
+            childElement = document.createElement('span');
+            childElement.appendChild(displayNode);
+
+            anLI = document.createElement('li');
+            anLI.appendChild(childElement);
+            breadcrumbList.appendChild(anLI);
+
+            // add class to body so we know its the list page
+            newclassName = ' listpage';
+            document.body.className += newclassName;
+        }
+    }
+
+
+    // fourth breadcrumb
+    if (testElement !== null) {
+        // an event class means we are on a detail page
+
+        // for desktop, display the event's title as an unlinked breadcrumb
+        // for mobile, display 'event details' - some of the titles are long
+        anLI = document.createElement('li');
+
+        const mobileLabel = 'Event details';
+        displayNode = document.createTextNode(mobileLabel);
+        childElement = document.createElement('span');
+        childElement.className = 'mobileOnly';
+        childElement.appendChild(displayNode);
+        anLI.appendChild(childElement);
+
+        const textProperty = 'textContent' in document ? 'textContent' : 'innerText';
+        const nonMobileLabel = testElement[textProperty];
+        displayNode = document.createTextNode(nonMobileLabel);
+        childElement = document.createElement('span');
+        childElement.className = 'nonMobile';
+        childElement.appendChild(displayNode);
+        anLI.appendChild(childElement);
+
+        breadcrumbList.appendChild(anLI);
+    }
+
+    parentBlock.insertBefore(breadcrumbList, parentBlock.firstChild);
+
+    return true;
 }
 
 /**
  * find the specific link on the page and relabel it
  */
 function updateEventsLinkText() {
-  //select a link to more events from the sidebar (upcoming events)
-  var moreEventsLink = document.querySelector('.sidebar .body a[href$="/events"]');
-  if (moreEventsLink !== null) {
-    // this has to be in all caps to make the nightwatch tests pass - Edge browser doesnt recognise a css transform
-    moreEventsLink.innerHTML = "MORE EVENTS";
-  }
+    //select a link to more events from the sidebar (upcoming events)
+    const moreEventsLink = document.querySelector('.sidebar .body a[href$="/events"]');
+    if (moreEventsLink !== null) {
+        // this has to be in all caps to make the nightwatch tests pass - Edge browser doesnt recognise a css transform
+        moreEventsLink.innerHTML = "MORE EVENTS";
+    }
 }
 
 ready(loadReusableComponents);
--- src/applications/studenthub/readme.md
+++ src/applications/studenthub/readme.md
@@ -4,12 +4,10 @@ A third party product that we are theming to match the general library theme.
 
 Current area is the [Library Staff Development workgroup portal](https://studenthub.uq.edu.au/workgroups/library-staff-development/events/). We are theming this by putting our polymer reusable elements in the top of the body of the page via a GUI editor.
 
-*Editing in the studenthub GUI interface does not return all the html lines that were supplied*, so start with the code block below - dont try to edit in place in the GUI.
-
 Method to edit the theme:
 
 - Decide what changes are needed and update below
-- Visit the theme edit page [current link](https://www.studenthub.uq.edu.au/Admin/SubSites/Layout.aspx?id=14) or: login > left hand menu, click on Work Groups > centre block, click on name of Work Group > right hand menu, click on Work group settings > middle area, click on Layout
+- Visit the theme edit page [current link](https://www.studenthub.uq.edu.au/Admin/SubSites/Layout.aspx?id=14) or check "Locating the Theme Edit page" below
 - Click on the word '(text)' in the header of the example-layout (if this isnt available, drag the 'text' item from
   the layout options into the header field, then click)
 - Click on the angle bracket icon ('<>') - a very short area will load with white markup on a black background. This is the editing area
@@ -18,17 +16,29 @@ Method to edit the theme:
 - Click OK
 - Reload the workgroup portal page to confirm (the change should be instant)
 
+**Editing in the studenthub GUI interface does not return all the html lines that were saved**, so start with the code block below - dont try to edit in place in the GUI.
+
 Code to include in the GUI editor (keep this up to date as it can't be reviewed reliably in the GUI):
 
         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0">
-        <script src="//assets.library.uq.edu.au/reusable-components/resources/preloader.js" async></script>
-        <script src="//assets.library.uq.edu.au/reusable-components/webcomponentsjs/webcomponents-lite.js" async></script>
-        <link rel="import" href="//assets.library.uq.edu.au/reusable-components/elements.vulcanized.html" async>    
-        <script src="//assets.library.uq.edu.au/reusable-components/careerhub/load.js" async></script>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-        <link rel="stylesheet" href="//assets.library.uq.edu.au/reusable-components/careerhub/custom-styles.css">
+        <script src="https://assets.library.uq.edu.au/reusable-webcomponents-development/feature-studenthub/applications/studenthub/load.js" async></script>
+        <link rel="stylesheet" type="text/css" href="https://static.uq.net.au/v6/fonts/Roboto/roboto.css" />
+        <link rel="stylesheet" type="text/css" href="https://static.uq.net.au/v9/fonts/Merriweather/merriweather.css" />
+        <link rel="stylesheet" type="text/css" href="https://static.uq.net.au/v13/fonts/Montserrat/montserrat.css">
+        <script type="text/javascript" src="https://assets.library.uq.edu.au/reusable-webcomponents/uq-lib-reusable.min.js" defer></script>
+        <link rel="stylesheet" href="https://assets.library.uq.edu.au/reusable-webcomponents-development/feature-studenthub/applications/custom-styles.css">
 
 Notes:
 
 * Careerhub have said they will put the meta viewport line in the template. It hasnt appeared yet - if it does, this line can be removed here
 * Material design icons are being used, so the font family is included
+
+### Locating the Theme Edit page
+
+The theme edit page is here [current link](https://www.studenthub.uq.edu.au/Admin/SubSites/Layout.aspx?id=14) or follow these steps:
+
+- login
+- left hand menu, click on Work Groups, click on list
+- centre block, click on name of Work Group
+- right hand menu, click on Settings
+- open Legacy Settings, click on Layout
