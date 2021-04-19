665d57f188c8e383e0696c4c4f05cd22f1407b7e
Lea de Groot <l.degroot@uq.edu.au>
2021-04-19 11:35:14 +1000

    add old scss to rightnow [PT #177032853]

--- webpack.config.js
+++ webpack.config.js
@@ -123,6 +123,7 @@ module.exports = () => {
                     {from: "src/favicon.ico", to: "favicon.ico"},
                     // all load.js for applications should be included here
                     {from: "src/applications/drupal/load.js", to: "applications/drupal/load.js"},
+                    {from: "src/applications/rightnow/load.js", to: "applications/rightnow/load.js"},
                     {from: "src/applications/shared/load.js", to: "applications/shared/load.js"},
                     {from: "src/applications/studenthub/load.js", to: "applications/studenthub/load.js"},
                     {from: "src/applications/uqlapp/load.js", to: "applications/uqlapp/load.js"},
