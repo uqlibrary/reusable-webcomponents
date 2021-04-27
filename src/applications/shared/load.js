3570cbfa189c716a91d420cbfd38d63188c378b0
Lea de Groot <l.degroot@uq.edu.au>
2021-04-27 15:14:26 +1000

    add gtm manually now we arent using a default [PT #177880272]

--- src/applications/shared/load.js
+++ src/applications/shared/load.js
@@ -14,6 +14,7 @@ function loadReusableComponents() {
 
     if (!document.querySelector('uq-gtm')) {
         const gtm = document.createElement('uq-gtm');
+        !!gtm && gtm.setAttribute("gtm", "GTM-W4KK37");
         !!gtm && document.body.insertBefore(gtm, firstElement);
     }
 
