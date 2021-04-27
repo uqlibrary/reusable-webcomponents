10cddfb1c2637dd76439dd69a6ddae871eea0cf2
Ky Lane <ky@lanedigital.com>
2021-04-27 10:46:15 +1000

    Fix for GTM to wait for a attr before rendering

--- src/applications/testing/load.js
+++ src/applications/testing/load.js
@@ -9,6 +9,10 @@ function ready(fn) {
 function loadReusableComponents() {
     const firstElement = document.body.children[0];
 
+    const gtm = document.createElement('uq-gtm');
+    gtm.setAttribute('gtm', 'ABC123');
+    document.body.insertBefore(gtm, firstElement);
+
     const header = document.createElement('uq-header');
     document.body.insertBefore(header, firstElement);
 
