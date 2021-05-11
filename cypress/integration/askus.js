0f4c14fe11d33040f28e97800b6f7714acd33b3e
Ky Lane <ky@lanedigital.com>
2021-05-11 12:03:40 +1000

    AskUsButton coverage

--- cypress/integration/askus.js
+++ cypress/integration/askus.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+//TODO: Need to stub the mock API response to test chat offline etc.
+
 describe('AskUs menu', () => {
     context('AskUs Menu', () => {
         it('Appears as expected', () => {
@@ -28,6 +30,42 @@ describe('AskUs menu', () => {
                 scopeName: 'Accessibility',
                 includedImpacts: ['minor', 'moderate', 'serious', 'critical'],
             });
+        });
+
+        it('On primo it doesnt show the blanking pane', () => {
+            cy.visit('http://localhost:8080/index-primo.html');
+            cy.viewport(1280, 900);
+            cy.get('askus-button').shadow().find('button#askus-button').click();
+            cy.get('askus-button').shadow().find('div#askus-pane').should('have.class', 'noOpacity');
+        });
+
+        it('Pressing esc closes the askus menu', () => {
+            cy.visit('http://localhost:8080');
+            cy.viewport(1280, 900);
+            cy.get('askus-button').shadow().find('button#askus-button').click();
+            cy.wait(500);
+            cy.get('askus-button').shadow().find('div#askus-menu').should('be.visible');
+            cy.get('body').type('{esc}', { force: true })
+            cy.wait(500);
+            cy.get('askus-button').shadow().find('div#askus-menu').should('not.be.visible');
+        });
+        it('Clicking the pane closes the askus menu', () => {
+            cy.visit('http://localhost:8080');
+            cy.viewport(1280, 900);
+            cy.get('askus-button').shadow().find('button#askus-button').click();
+            cy.wait(500);
+            cy.get('askus-button').shadow().find('div#askus-menu').should('be.visible');
+            cy.get('askus-button').shadow().find('div#askus-pane').click();
+            cy.wait(500);
+            cy.get('askus-button').shadow().find('div#askus-menu').should('not.be.visible');
+        });
+
+        it('Navigates to contact from askus menu', () => {
+            cy.visit('http://localhost:8080');
+            cy.viewport(1280, 900);
+            cy.get('askus-button').shadow().find('button#askus-button').click();
+            cy.get('askus-button').shadow().find('a[data-testid="askus-menu-moreways"').click();
+            cy.location('href').should('eq', 'https://web.library.uq.edu.au/contact-us');
         });
     });
 
@@ -62,9 +100,10 @@ describe('AskUs menu', () => {
             cy.get('askus-button').shadow().find('div#askus-proactive-chat').should('have.class', 'show');
             cy.get('askus-button').shadow().find('button#askus-proactive-chat-button-close').click();
             cy.getCookie('UQ_ASKUS_PROACTIVE_CHAT').should('have.property', 'value', 'hidden');
+            cy.get('askus-button').shadow().find('#askus-proactive-chat-wrapper').should('not.exist');
         });
 
-        it('Can opens chat window', () => {
+        it('Can open chat window', () => {
             cy.visit('http://localhost:8080', {
                 onBeforeLoad(win) {
                     cy.stub(win, 'open');
