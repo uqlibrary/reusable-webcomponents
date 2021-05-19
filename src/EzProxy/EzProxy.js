ee347fbed147e2d609860940f61e25635242e84e
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2021-05-19 10:26:37 +1000

    [#178123311] Copy generated styles from UQDS for Grid, Body, Button, Card and Form

--- src/EzProxy/EzProxy.js
+++ src/EzProxy/EzProxy.js
@@ -1,3 +1,4 @@
+import mainStyles from './css/main.css';
 import customStyles from './css/overrides.css';
 
 /*
@@ -10,17 +11,18 @@ import customStyles from './css/overrides.css';
 const template = document.createElement('template');
 template.innerHTML = `
     <style>
+        ${mainStyles.toString()}
         ${customStyles.toString()}
     </style>
-    <div id="ez-proxy">
-        <fieldset>
+    <div id="ez-proxy" class="uq-grid">
+        <fieldset class="uq-grid__col--5">
             <input type="url" placeholder="DOI or URL" id="ez-proxy-input" />
-            <textarea id="ez-proxy-url-display-area"></textarea>
-            <button id="ez-proxy-create-link-button">Create Link</button>
-            <button id="ez-proxy-create-new-link-button">Create New Link</button>
-            <button id="ez-proxy-test-link-button">Test Link</button>
-            <button id="ez-proxy-copy-link-button">Copy Link</button>
-            <button id="ez-proxy-redirect-button">Go</button>
+            <textarea id="ez-proxy-url-display-area" class="hidden"></textarea>
+            <button id="ez-proxy-create-link-button" class="uq-button hidden">Create Link</button>
+            <button id="ez-proxy-create-new-link-button" class="uq-button hidden">Create New Link</button>
+            <button id="ez-proxy-test-link-button" class="uq-button hidden">Test Link</button>
+            <button id="ez-proxy-copy-link-button" class="uq-button hidden">Copy Link</button>
+            <button id="ez-proxy-redirect-button" class="uq-button hidden">Go</button>
         </fieldset>
     </div>
 `;
