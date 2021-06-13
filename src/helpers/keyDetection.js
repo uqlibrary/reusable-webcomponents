253a2cb3906d0cd07d71c7a64d1267b2037dcf27
Lea de Groot <l.degroot@uq.edu.au>
2021-06-14 08:26:31 +1000

    provide reusable helpers for key detection [PT #178140544]

---
+++ src/helpers/keyDetection.js
@@ -0,0 +1,20 @@
+export function isKeyPressed(e, charKeyInput, numericKeyInput) {
+    const keyNumeric = e.charCode || e.keyCode;
+    const keyChar = e.key || e.code;
+    return keyChar === charKeyInput || keyNumeric === numericKeyInput;
+}
+export function isEscapeKeyPressed(e) {
+    return isKeyPressed(e, 'Escape', 27);
+}
+export function isArrowDownKeyPressed(e) {
+    return isKeyPressed(e, 'ArrowDown', 40);
+}
+export function isArrowUpKeyPressed(e) {
+    return isKeyPressed(e, 'ArrowUp', 38);
+}
+export function isReturnKeyPressed(e) {
+    return isKeyPressed(e, 'Enter', 13);
+}
+function isTabKeyPressed(e) {
+    return isKeyPressed(e, 'Tab', 9);
+}
