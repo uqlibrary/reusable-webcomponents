d796f797b586f2fd606a3b7d41b5adc03ec1ef98
Lea de Groot <l.degroot@uq.edu.au>
2021-06-29 08:24:16 +1000

    handle mock promise [PT #178346835]

--- src/ApiAccess/ApiAccess.js
+++ src/ApiAccess/ApiAccess.js
@@ -148,10 +148,9 @@ class ApiAccess {
             .then((data) => {
                 return (
                     (data &&
-                        data.body &&
-                        data.body.response &&
-                        data.body.response.docs &&
-                        data.body.response.docs.map((item, index) => {
+                        data.response &&
+                        data.response.docs &&
+                        data.response.docs.map((item, index) => {
                             return {
                                 ...item,
                                 index,
@@ -213,10 +212,7 @@ class ApiAccess {
         console.log('fetchiong: ', new ApiRoutes().SECURE_COLLECTION_CHECK_API({ path }).apiUrl);
         return await this.fetchAPI(new ApiRoutes().SECURE_COLLECTION_CHECK_API({ path }).apiUrl, {}, true, false)
             .then((data) => {
-                console.log('data = ', data);
-                return !!data.response || !!data.url
-                    ? data // mock
-                    : /* istanbul ignore next */ data.json(); // live
+                return data;
             })
             .catch((error) => {
                 console.log('error loading Secure Collection Check ', error);
@@ -257,18 +253,33 @@ class ApiAccess {
             ...headers,
         };
 
-        // reference: https://dmitripavlutin.com/javascript-fetch-async-await/
-        const response = await this.fetchFromServer(urlPath, options, timestampRequired);
-        console.log('fetchAPI: response = ', response);
-        if (!response.ok) {
-            console.log(`ApiAccess console: An error has occured: ${response.status} ${response.statusText}`);
-            const message = `ApiAccess: An error has occured: ${response.status} ${response.statusText}`;
-            throw new Error(message);
+        /* istanbul ignore else  */
+        if (this.isMock()) {
+            try {
+                return this.fetchMock(urlPath);
+            } catch (e) {
+                const msg = `mock api error: ${e.message}`;
+                console.log(msg);
+                throw new Error(msg);
+            }
+        } else {
+            // reference: https://dmitripavlutin.com/javascript-fetch-async-await/
+            const API_URL = process.env.API_URL || 'https://api.library.uq.edu.au/staging/';
+            const connector = urlPath.indexOf('?') > -1 ? '&' : '?';
+            const addTimestamp = !!timestampRequired ? `${connector}${new Date().getTime()}` : '';
+
+            const response = fetch(`${API_URL}${urlPath}${addTimestamp}`, {
+                headers: options,
+            });
+            console.log('response = ', response);
+
+            if (!response.ok) {
+                console.log(`ApiAccess console: An error has occured: ${response.status} ${response.statusText}`);
+                const message = `ApiAccess: An error has occured: ${response.status} ${response.statusText}`;
+                throw new Error(message);
+            }
+            return await response.json();
         }
-        return !!response.body
-            ? response.body // mock
-            : await response.json(); // live
-        // return await response.json();
     }
 
     async fetchJsonpAPI(url, headers) {
@@ -300,33 +311,15 @@ class ApiAccess {
         }
     }
 
-    fetchFromServer(url, options, timestampRequired = true) {
-        const API_URL = process.env.API_URL || 'https://api.library.uq.edu.au/staging/';
-        const connector = url.indexOf('?') > -1 ? '&' : '?';
-        const addTimestamp = !!timestampRequired ? `${connector}${new Date().getTime()}` : '';
-        const urlpath = `${url}${addTimestamp}`;
-        console.log('fetchFromServer: url = ', url);
-        console.log('fetchFromServer: urlpath = ', urlpath);
-        console.log('#### live url would be: ', `${API_URL}${urlpath}`);
-        /* istanbul ignore else */
-        if (this.isMock()) {
-            try {
-                const result = this.fetchMock(url);
-                console.log('mock gave ', result);
-                return result;
-            } catch (e) {
-                const msg = `mock api error: ${e.message}`;
-                console.log(msg);
-                throw new Error(msg);
-            }
-        } else {
-            const responsePromise = fetch(`${API_URL}${urlpath}`, {
-                headers: options,
-            });
-            console.log('responsePromise = ', responsePromise);
-            return responsePromise;
-        }
-    }
+    /* istanbul ignore next */
+    // fetchFromServer(urlPath, options) {
+    //     const API_URL = process.env.API_URL || 'https://api.library.uq.edu.au/staging/';
+    //     const connector = urlPath.indexOf('?') > -1 ? '&' : '?';
+    //
+    //     return fetch(`${API_URL}${urlPath}${connector}${new Date().getTime()}`, {
+    //         headers: options,
+    //     });
+    // }
 
     storeAccount(account, numberOfHoursUntilExpiry = 8) {
         // for improved UX, expire the session storage when the token must surely be expired, for those rare long sessions
@@ -397,20 +390,19 @@ class ApiAccess {
     }
 
     fetchMock(url, options = null) {
-        return new Promise((resolve, reject) => {
-            const response = new MockApi().mockfetch(url, options);
-            console.log('mock url = ', url);
-            console.log('mock response = ', response);
-            if (!response.ok || !response.body) {
-                const msg = `fetchMock: An error has occured in mock for ${url}: ${response.status}`;
-                console.log(msg);
-                throw new Error(msg);
-            }
-            setTimeout(() => resolve(response), 50);
-        });
-
-        // console.log(url, ' stringify = ', JSON.stringify(response));
-        // return JSON.stringify(response) || /* istanbul ignore next */ {};
+        const response = new MockApi().mockfetch(url, options);
+        console.log('mock url = ', url);
+        console.log('mock response = ', response);
+        // if (url.startsWith('file/collection/')) {
+        //     // the secure collections give a different formatted response and handles its own errors
+        //     return response;
+        // } else
+        if (!response.ok || !response.body) {
+            const msg = `fetchMock: An error has occured in mock for ${url}: ${response.status}`;
+            console.log(msg);
+            throw new Error(msg);
+        }
+        return response.body || /* istanbul ignore next */ {};
     }
 
     isMock() {
