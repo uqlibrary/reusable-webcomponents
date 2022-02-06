3b6992f8564c8d95e2e272952b6a996f75d148d4
Lea de Groot <l.degroot@uq.edu.au>
2022-02-07 09:37:56 +1000

    remove the drupal link from homepage html file [PT #178735546]

--- webpack.config.js
+++ webpack.config.js
@@ -173,6 +173,16 @@ module.exports = () => {
                             },
                         ],
                     },
+                    {
+                        dir: buildPath(process.env.NODE_ENV, 'index'),
+                        files: ['index.html'],
+                        rules: [
+                            {
+                                search: /\<script type="text\/javascript" src="drupal-lib-reusable.min.js"><\/script>/gm,
+                                replace: '',
+                            },
+                        ],
+                    },
                 ]),
             new webpack.DefinePlugin({
                 __DEVELOPMENT__: true,
