01c9ed2447ad5e50b4d4d23618807781296a03ea
Lea de Groot <l.degroot@uq.edu.au>
2022-07-27 15:47:34 +1000

    remove now unused mylibrary code [PT #182204834]

--- src/UtilityArea/mylibrary.locale.js
+++
@@ -1,4 +0,0 @@
-export const mylibraryLocale = {
-    MYLIBRARY_STUB_ID: 'mylibrarystub',
-    MYLIBRARY_BUTTON_ID: 'mylibrarybutton',
-};
--- src/applications/libguides/load.js
+++ src/applications/libguides/load.js
@@ -23,26 +23,6 @@ function createSlotForButtonInUtilityArea(button, id = null) {
     return slot;
 }
 
-function createMylibraryStub() {
-    const stubId = 'mylibrarystub'; // match mylibraryLocale.MYLIBRARY_STUB_ID
-    if (!!document.getElementById(stubId)) {
-        return false;
-    }
-
-    const mylibraryButtonId = 'mylibrarybutton'; // match mylibraryLocale.MYLIBRARY_BUTTON_ID
-    if (!!document.getElementById(mylibraryButtonId)) {
-        return false;
-    }
-
-    // this just creates a stub - authbutton will insert the actual button if they are logged in when this stub is present
-    mylibraryButton = document.createElement('div');
-    mylibraryButton.id = stubId;
-
-    const slot = !!mylibraryButton && this.createSlotForButtonInUtilityArea(mylibraryButton, mylibraryButtonId);
-
-    return slot;
-}
-
 function createAuthButton() {
     if (!!document.querySelector('auth-button')) {
         return false;
@@ -95,9 +75,6 @@ function loadReusableComponentsLibGuides() {
     if (!isOutsideUQ) {
         const askusButton = createAskusButton();
         !!siteHeader && !!askusButton && siteHeader.appendChild(askusButton);
-
-        const mylibraryStub = createMylibraryStub();
-        !!siteHeader && !!mylibraryStub && siteHeader.appendChild(mylibraryStub);
 
         const authButton = createAuthButton();
         !!siteHeader && !!authButton && siteHeader.appendChild(authButton);
