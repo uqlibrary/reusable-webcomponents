c96aa8f3c709828feeaa1ceb552301b331bf138a
Lea de Groot <l.degroot@uq.edu.au>
2022-07-29 10:23:22 +1000

    typo [PT #182204834]

--- src/UtilityArea/AuthButton.js
+++ src/UtilityArea/AuthButton.js
@@ -62,6 +62,7 @@ authorisedtemplate.innerHTML = `
                             <a data-testid="mylibrary-menu-course-resources" href="https://www.library.uq.edu.au/learning-resources" rel="noreferrer">
                                 <svg class="MuiSvgIcon-root MuiSvgIcon-colorSecondary" focusable="false" viewBox="0 0 24 24" aria-hidden="true" style="margin-right: 6px; margin-bottom: -6px;"><path d="M5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82zM12 3L1 9l11 6 9-4.91V17h2V9L12 3z"></path></svg>
                                 <span>Learning resources</span>
+                                <div class="subtext">Course readings & exam papers</div>
                             </a>
                         </li>
                         
@@ -70,6 +71,7 @@ authorisedtemplate.innerHTML = `
                             <a data-testid="mylibrary-menu-print-balance" href="https://lib-print.library.uq.edu.au:9192/user" rel="noreferrer">
                                 <svg class="MuiSvgIcon-root MuiSvgIcon-colorSecondary" focusable="false" viewBox="0 0 24 24" aria-hidden="true" style="margin-right: 6px; margin-bottom: -6px;"><path d="M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z"></path></svg>
                                 <span>Print balance</span>
+                                <div class="subtext">Log in to your print account</div>
                             </a>
                         </li>
                         
@@ -77,7 +79,8 @@ authorisedtemplate.innerHTML = `
                         <li role="menuitem" aria-disabled="false">
                             <a data-testid="mylibrary-menu-room-bookings" href="https://uqbookit.uq.edu.au/#/app/booking-types/77b52dde-d704-4b6d-917e-e820f7df07cb" rel="noreferrer">
                                 <svg class="MuiSvgIcon-root MuiSvgIcon-colorSecondary" focusable="false" viewBox="0 0 24 24" aria-hidden="true" style="margin-right: 6px; margin-bottom: -6px;"><path d="M2 17h20v2H2zm11.84-9.21c.1-.24.16-.51.16-.79 0-1.1-.9-2-2-2s-2 .9-2 2c0 .28.06.55.16.79C6.25 8.6 3.27 11.93 3 16h18c-.27-4.07-3.25-7.4-7.16-8.21z"></path></svg>
-                                <span>Room bookings</span>
+                                <span>Book a room or desk</span>
+                                <div class="subtext">Student meeting & study spaces</div>
                             </a>
                         </li>
                     </ul>
