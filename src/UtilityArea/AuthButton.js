7fb860001bb5babff73ae0bd34a6bbff62a84af5
Lea de Groot <l.degroot@uq.edu.au>
2022-08-01 13:03:18 +1000

    tweak label [PT #182204834]

--- src/UtilityArea/AuthButton.js
+++ src/UtilityArea/AuthButton.js
@@ -54,7 +54,7 @@ authorisedtemplate.innerHTML = `
                             <a data-testid="mylibrary-menu-saved-items" href="https://search.library.uq.edu.au/primo-explore/login?vid=61UQ&targetURL=https%3A%2F%2Fsearch.library.uq.edu.au%2Fprimo-explore%2Ffavorites%3Fvid%3D61UQ%26lang%3Den_US%26section%3Ditems" rel="noreferrer">
                                 <svg class="MuiSvgIcon-root MuiSvgIcon-colorSecondary" focusable="false" viewBox="0 0 24 24" aria-hidden="true" style="margin-right: 6px; margin-bottom: -6px;"><path d="m12 21.35-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path></svg>
                                 <span>Favourites</span>
-                                <div class="subtext">Saved items & searches</div>
+                                <div class="subtext">Saved items, searches & search history</div>
                             </a>
                         </li>
 
