d69e0b6263c1f950b8334e2ea639c8cec95211ff
Steve Lancaster <90990468+SteveLancaster@users.noreply.github.com>
2023-02-06 15:16:21 +1000

    Menu item applied

--- src/UtilityArea/AuthButton.js
+++ src/UtilityArea/AuthButton.js
@@ -139,6 +139,15 @@ authorisedtemplate.innerHTML = `
                                     <span>Test and Tag</span>
                                 </a>
                             </li>
+
+                            <!-- Promo Panel Admin -->
+                            <li data-testid="promopanel-admin" id="promopanel-admin" role="menuitem" aria-disabled="false">
+                                <a tabindex="0" id="mylibrary-menu-promopanel-admin"  data-testid="mylibrary-menu-promopanel-admin" href="https://www.library.uq.edu.au/admin/promopanel" rel="noreferrer">
+                                
+                                <svg class="MuiSvgIcon-root MuiSvgIcon-colorSecondary" focusable="false" viewBox="0 0 24 24" aria-hidden="true" style="margin-right: 6px; margin-bottom: -6px;"><path d="M18 11v2h4v-2h-4zm-2 6.61c.96.71 2.21 1.65 3.2 2.39.4-.53.8-1.07 1.2-1.6-.99-.74-2.24-1.68-3.2-2.4-.4.54-.8 1.08-1.2 1.61zM20.4 5.6c-.4-.53-.8-1.07-1.2-1.6-.99.74-2.24 1.68-3.2 2.4.4.53.8 1.07 1.2 1.6.96-.72 2.21-1.65 3.2-2.4zM4 9c-1.1 0-2 .9-2 2v2c0 1.1.9 2 2 2h1v4h2v-4h1l5 3V6L8 9H4zm11.5 3c0-1.33-.58-2.53-1.5-3.35v6.69c.92-.81 1.5-2.01 1.5-3.34z"></path></svg>
+                                    <span>Promo Panel</span>
+                                </a>
+                            </li>
             
                         </ul>
                     </div>
@@ -214,6 +223,9 @@ class AuthButton extends HTMLElement {
 
                 const testTagAdminElement = !!shadowDOM && shadowDOM.getElementById('testTag-admin');
                 !this.canSeeTestTagAdmin(account) && !!testTagAdminElement && testTagAdminElement.remove();
+
+                const promoPanelElement = !!shadowDOM && shadowDOM.getElementById('promopanel-admin');
+                !this.canSeePromopanelAdmin(account) && !!promoPanelElement && promoPanelElement.remove();
 
                 this.showHideMylibraryEspaceOption(shadowDOM);
 
@@ -348,6 +360,14 @@ class AuthButton extends HTMLElement {
         }
 
         // these ifs must match the reverse order of display
+        if (this.canSeePromopanelAdmin(account)) {
+            !!shadowDOM &&
+                shadowDOM.getElementById('mylibrary-menu-promopanel-admin').addEventListener('keydown', function (e) {
+                    if (isTabKeyPressed(e)) {
+                        closeAccountOptionsMenu();
+                    }
+                });
+        }
         if (this.canSeeTestTagAdmin(account)) {
             !!shadowDOM &&
                 shadowDOM.getElementById('mylibrary-menu-testTag-admin').addEventListener('keydown', function (e) {
@@ -443,6 +463,10 @@ class AuthButton extends HTMLElement {
 
     canSeeTestTagAdmin(account) {
         return !!account && !!this.hasTestTagAdminAccess(account);
+    }
+
+    canSeePromopanelAdmin(account) {
+        return !!account && !!this.hasWebContentAdminAccess(account);
     }
 
     async showHideMylibraryEspaceOption(shadowDOM) {
