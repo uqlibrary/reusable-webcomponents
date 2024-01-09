c326c5fc8ce6859cf5f31bf47f76b94588f90336
Lea de Groot <l.degroot@glibrary.uq.edu.au>
2024-01-10 09:44:58 +1000

    put video call after phone number in CRM sidebar [PT #186572318]

--- src/applications/rightnow/load.js
+++ src/applications/rightnow/load.js
@@ -104,9 +104,7 @@ function addVideoCallLinkToSidebar() {
         </span>
     </li>`;
 
-    const askusEmailLink = document.querySelector(
-        '#uq_AskQuestionContent .menu a[href="mailto:askus@library.uq.edu.au"]',
-    );
+    const askusEmailLink = document.querySelector('#uq_AskQuestionContent .menu a[href="tel:61733464312"]');
     const emailListItem = !!askusEmailLink && askusEmailLink.parentNode.parentNode;
     !!emailListItem && emailListItem.insertAdjacentHTML('afterend', newHtml);
 }
