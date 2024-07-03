14dde779ec7b7f45716a27ee19e62cb93cf7bef3
Lea de Groot <l.degroot@library.uq.edu.au>
2024-07-03 10:50:59 +1000

    initial reskin of askus button [#187866122]

--- src/UtilityArea/AskUsButton.js
+++ src/UtilityArea/AskUsButton.js
@@ -21,7 +21,7 @@ template.innerHTML = `
             <svg id="askus-icon" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
                 <path d="M21 6h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1zm-4 6V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v14l4-4h10c.55 0 1-.45 1-1z"/>
             </svg>
-            <div id="askus-label" part="label">AskUs</div>
+            <span id="askus-label" part="label">AskUs</span>
         </button>
         <!-- Menu -->
         <div id="askus-menu" data-testid="askus-menu" data-analyticsid="askus-menu" class="closed-menu" style="display: none">
