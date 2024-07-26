81560a749ca6991195bc5ae67e4e4f06a8ca6a83
Lea de Groot <l.degroot@library.uq.edu.au>
2024-07-26 12:01:29 +1000

    adjust footer [PT #187889028]

--- src/ConnectFooter/ConnectFooter.js
+++ src/ConnectFooter/ConnectFooter.js
@@ -5,10 +5,14 @@ const template = document.createElement('template');
 template.innerHTML = `
     <style>${styles.toString()}</style>
     <div data-analyticsid="connect-footer" id="connect-footer" class="connect-footer connectfooter-wrapper griditem griditem12" data-gtm-category="Footer">
-        <div class="uq-footer__container connectfooter-wrapper2 griditem griditem12">
+        <div class="uq-footer__container griditem griditem12"> <!--  connectfooter-wrapper2 -->
             <div class="connectfooter-wrapper3">
                 <div class="layout-card">
-                <p data-testid="connect-footer-social-heading">Library footer with social / giving options - details to be determined.</p>
+                    <div class="gridcontainer flexstart" id="connect-footer-block">
+                        <div class="griditem">
+                            <p data-testid="connect-footer-social-heading">Library footer with social / giving options - details to be determined.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
