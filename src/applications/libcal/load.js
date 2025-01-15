8c681fc6a85ac175c17632e8e558f0cf971033b9
Lea de Groot <l.degroot@library.uq.edu.au>
2025-01-15 14:00:02 +1000

    refactor [PT #188749609]

--- src/applications/libcal/load.js
+++ src/applications/libcal/load.js
@@ -19,67 +19,48 @@ function ready(fn) {
     }
 }
 
-let isOutsideUQ = true;
-
-if (window.location.href.indexOf('uq.edu.au') > -1) {
-    isOutsideUQ = false;
-}
-
-function createSlotForButtonInUtilityArea(button, id = null) {
-    const slot = document.createElement('span');
-    !!slot && slot.setAttribute('slot', 'site-utilities');
-    !!slot && !!id && slot.setAttribute('id', id);
-    !!button && !!slot && slot.appendChild(button);
-
-    return slot;
-}
-
-function createAuthButton() {
-    if (!!document.querySelector('auth-button')) {
-        return false;
+function loadReusableComponentsLibGuides() {
+    function isOutsideUQ() {
+        return window.location.href.indexOf('uq.edu.au') <= -1;
     }
 
-    const authButton = document.createElement('auth-button');
-    const slot = !!authButton && createSlotForButtonInUtilityArea(authButton, 'auth');
+    function createSlotForButtonInUtilityArea(button, id = null) {
+        const slot = document.createElement('span');
+        !!slot && slot.setAttribute('slot', 'site-utilities');
+        !!slot && !!id && slot.setAttribute('id', id);
+        !!button && !!slot && slot.appendChild(button);
 
-    return slot;
-}
-
-function createElement(type, props) {
-    var $e = document.createElement(type);
-    for (var prop in props) {
-        $e.setAttribute(prop, props[prop]);
+        return slot;
     }
-    return $e;
-}
 
-function fontLoader(font) {
-    var headID = document.getElementsByTagName('head')[0];
-    var link = document.createElement('link');
-    link.type = 'text/css';
-    link.rel = 'stylesheet';
-    headID.appendChild(link);
-    link.href = font;
-}
+    function createAuthButton() {
+        if (!!document.querySelector('auth-button')) {
+            return false;
+        }
 
-function moveSpringshareBreadcrumbsToSiteHeader(siteHeader) {
-    // dev, dont run this until we are happy with it
-    if (getSearchParam('override') === 'on' && getSearchParam('test') === 'yes') {
-        // run the code
-    } else {
-        return;
+        const authButton = document.createElement('auth-button');
+        return !!authButton && createSlotForButtonInUtilityArea(authButton, 'auth');
+    }
+
+    function fontLoader(font) {
+        var headID = document.getElementsByTagName('head')[0];
+        var link = document.createElement('link');
+        link.type = 'text/css';
+        link.rel = 'stylesheet';
+        headID.appendChild(link);
+        link.href = font;
     }
     function breadcrumblink(b) {
         // truncate the breadcrumb title manually because updating the springshare setting has Consequences
         const title = isCalendarHomepage(b.href) ? 'Calendar' : b.title;
         return `<li class="uq-breadcrumb__item">
-                <a class="uq-breadcrumb__link" title="${b.title}" href="${b.href}">${title}</a>
-                </li>`;
+                    <a class="uq-breadcrumb__link" title="${b.title}" href="${b.href}">${title}</a>
+                    </li>`;
     }
     function breadcrumbSpan(title) {
         return `<li class="uq-breadcrumb__item">
-                <span class="uq-breadcrumb__link" title="${title}">${title}</span>
-                </li>`;
+                    <span class="uq-breadcrumb__link" title="${title}">${title}</span>
+                    </li>`;
     }
     function isLibraryHomepage(href) {
         return (
@@ -97,31 +78,40 @@ function moveSpringshareBreadcrumbsToSiteHeader(siteHeader) {
             href !== 'http://calendar.library.uq.edu.au'
         );
     }
-    const awaitSiteHeader = setInterval(() => {
-        const siteHeaderShadowRoot = siteHeader.shadowRoot;
-        if (!!siteHeaderShadowRoot) {
-            clearInterval(awaitSiteHeader);
-
-            const breadcrumbNav = document.querySelector('#s-lc-public-bc nav');
-            const listItems = !!breadcrumbNav && breadcrumbNav.querySelectorAll('ol li');
-
-            const breadcrumbParent = !!siteHeaderShadowRoot && siteHeaderShadowRoot.getElementById('breadcrumb_nav');
-            !!listItems &&
-                listItems.forEach((item) => {
-                    const anchor = item.querySelector('a');
-                    const title = anchor ? anchor.textContent : item.textContent;
-                    const href = anchor ? anchor.href : null;
-                    if (!isLibraryHomepage(href)) {
-                        const listItemEntry = !!href ? breadcrumblink({ title, href }) : breadcrumbSpan(title);
-                        breadcrumbParent.insertAdjacentHTML('beforeend', listItemEntry);
-                    }
-                });
-            !!breadcrumbNav && breadcrumbNav.remove();
+    function moveSpringshareBreadcrumbsToSiteHeader(siteHeader) {
+        // dev, dont run this until we are happy with it
+        if (getSearchParam('override') === 'on' && getSearchParam('test') === 'yes') {
+            // run the code
+        } else {
+            return;
         }
-    }, 100);
-}
 
-function loadReusableComponentsLibGuides() {
+        const awaitSiteHeader = setInterval(() => {
+            const siteHeaderShadowRoot = siteHeader.shadowRoot;
+            if (!!siteHeaderShadowRoot) {
+                clearInterval(awaitSiteHeader);
+
+                const breadcrumbNav = document.querySelector('#s-lc-public-bc nav');
+                const listItems = !!breadcrumbNav && breadcrumbNav.querySelectorAll('ol li');
+
+                const breadcrumbParent =
+                    !!siteHeaderShadowRoot && siteHeaderShadowRoot.getElementById('breadcrumb_nav');
+                !!listItems &&
+                    listItems.forEach((item) => {
+                        const anchor = item.querySelector('a');
+                        const title = anchor ? anchor.textContent : item.textContent;
+                        const href = anchor ? anchor.href : null;
+                        if (!isLibraryHomepage(href)) {
+                            const listItemEntry = !!href ? breadcrumblink({ title, href }) : breadcrumbSpan(title);
+                            breadcrumbParent.insertAdjacentHTML('beforeend', listItemEntry);
+                        }
+                    });
+                !!breadcrumbNav && breadcrumbNav.remove();
+            }
+        }, 100);
+    }
+
+    // start of page update
     fontLoader('https://static.uq.net.au/v15/fonts/Roboto/roboto.css');
     fontLoader('https://static.uq.net.au/v15/fonts/Merriweather/merriweather.css');
     fontLoader('https://static.uq.net.au/v15/fonts/Montserrat/montserrat.css');
@@ -140,7 +130,7 @@ function loadReusableComponentsLibGuides() {
     const siteHeader = document.createElement('uq-site-header');
     siteHeader.setAttribute('skipnavid', 's-lc-public-main');
 
-    if (!isOutsideUQ) {
+    if (!isOutsideUQ()) {
         const authButton = createAuthButton();
         !!siteHeader && !!authButton && siteHeader.appendChild(authButton);
     }
