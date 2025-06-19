f139b13c6661ec5e13a05aa291e427b98d87c4fe
Lea de Groot <l.degroot@library.uq.edu.au>
2025-06-20 09:32:41 +1000

    control style removal [Youtrack AD-118]

--- src/applications/libguides/load.js
+++ src/applications/libguides/load.js
@@ -879,11 +879,14 @@
             });
     }
 
-    const cssList = document.querySelectorAll('link[href][href$=".css"]');
-    !!cssList && cssList.length > 0 && cssList.forEach((css) => css.remove());
+    if (searchParameters.getValue('clearcss') === 'on') {
+        const cssList = document.querySelectorAll('link[href][href$=".css"]');
 
-    const styles = document.querySelectorAll('style');
-    !!styles && styles.length > 0 && styles.forEach((css) => css.remove());
+        !!cssList && cssList.length > 0 && cssList.forEach((css) => css.remove());
+
+        const styles = document.querySelectorAll('style');
+        !!styles && styles.length > 0 && styles.forEach((css) => css.remove());
+    }
 
     ready(applyUQLItemsToGuides);
 })();
