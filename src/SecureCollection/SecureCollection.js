e15dfae95a729c704d481c73ccae26726261e06a
Lea de Groot <l.degroot@library.uq.edu.au>
2025-06-25 09:35:39 +1000

    bring the looknfeel up to 2025 DS while I am here [Youtrack AD-197]

--- src/SecureCollection/SecureCollection.js
+++ src/SecureCollection/SecureCollection.js
@@ -26,20 +26,27 @@ circularProgressElement.innerHTML = `
 const template = document.createElement('template');
 template.innerHTML = `
     <style>${overrides.toString()}</style>
-    <div class="root MuiGrid-root MuiGrid-container" data-testid="secure-collection" id="StandardPage">
-        <div class="secure-collection-container">
-            <div class="MuiGrid-root jss163 MuiGrid-item MuiGrid-grid-xs-true">
-                <h2 class="MuiTypography-root MuiTypography-h4 MuiTypography-colorPrimary"
-                    data-testid="StandardPage-title"
-                >
-                    <span>Secure Collection</span>
-                </h2>
-            </div>
-            <div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12">
-                <section aria-live="assertive">
-                    <div id="block" class="contentbox MuiPaper-root MuiCard-root jss196 StandardCard MuiPaper-elevation1 MuiPaper-rounded StandardCard" data-testid="standard-card-copyright-notice" id="standard-card-copyright-notice">
+    <div>
+        <div id="library-hero" class="block block-system block-system-main-block" data-testid="hero-wrapper">
+                <div>
+                    <div class="uq-hero">
+                        <div class="uq-container">
+                            <div class="uq-hero__content" data-testid="hero-words-words-wrapper">
+                                <h1 class="uq-hero__title" data-testid="hero-text">Secure collection</h1>
+                                <!-- <div class="uq-hero__description"></div> -->
+                            </div>
+                        </div>
                     </div>
-                </section>
+                </div>
+        </div>
+        <div class="root MuiGrid-root MuiGrid-container" data-testid="secure-collection" id="StandardPage">
+            <div class="secure-collection-container">
+                <div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12">
+                    <section aria-live="assertive">
+                        <div id="block" class="contentbox MuiPaper-root MuiCard-root jss196 StandardCard MuiPaper-elevation1 MuiPaper-rounded StandardCard" data-testid="standard-card-copyright-notice" id="standard-card-copyright-notice">
+                        </div>
+                    </section>
+                </div>
             </div>
         </div>
     </div>
@@ -213,9 +220,9 @@ class SecureCollection extends HTMLElement {
         const commercialCopyrightAcknowledgementPanel = document.createElement('template');
         commercialCopyrightAcknowledgementPanel.innerHTML = `
 <p>This file is provided to support teaching and learning for the staff and students of the University of Queensland</p>
-<h3>COMMONWEALTH OF AUSTRALIA</h3>
-<h4>Copyright Regulations 1969</h4>
-<h5>WARNING</h5>
+<h2>COMMONWEALTH OF AUSTRALIA</h2>
+<h3>Copyright Regulations 1969</h3>
+<h4>WARNING</h4>
 <p>
     This material has been reproduced and communicated to you by or on behalf of the University of Queensland pursuant
     to Part VB of the Copyright Act 1968 (the Act).
@@ -268,7 +275,7 @@ class SecureCollection extends HTMLElement {
         block.appendChild(statutoryCopyrightAcknowledgementPanel.content.cloneNode(true));
         this.appendExtensionsSavePrompt(block);
 
-        this.wrapFragmentInStandardPage(block, 'WARNING');
+        this.wrapFragmentInStandardPage(block, 'Warning');
     }
 
     /**
@@ -433,22 +440,18 @@ class SecureCollection extends HTMLElement {
 
         if (!!title) {
             const titleNode = document.createTextNode(title);
-            const h3 = document.createElement('h3');
-            h3.className =
-                'MuiTypography-root MuiCardHeader-title MuiTypography-h5 MuiTypography-colorInherit MuiTypography-displayBlock';
-            h3.appendChild(titleNode);
+            const h2Element = document.createElement('h2');
+            h2Element.className =
+                'uqds-typography-root MuiCardHeader-title uqds-h2 MuiTypography-colorInherit MuiTypography-displayBlock';
+            h2Element.appendChild(titleNode);
 
             const wrapper = document.createElement('div');
             wrapper.className = 'MuiCardHeader-root wrapper';
-            wrapper.appendChild(h3);
+            wrapper.appendChild(h2Element);
             block.appendChild(wrapper);
         }
 
         const blockwrapper = document.createElement('div');
-        blockwrapper.style.marginBottom = '24px';
-        blockwrapper.style.paddingLeft = '24px';
-        blockwrapper.style.paddingRight = '24px';
-        blockwrapper.style.paddingBottom = '24px';
         blockwrapper.appendChild(fragment);
         block.appendChild(blockwrapper);
     }
