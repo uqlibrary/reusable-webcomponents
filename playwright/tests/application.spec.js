37e6d38c667889bf5d359b9c77c2b5dcd0d46671
uqmmoise <1010930+sk33wiff@users.noreply.github.com>
2025-11-12 15:15:57 +1000

    - refactoring Alerts to match DS Alert's style [AD-548]

--- playwright/tests/application.spec.js
+++ playwright/tests/application.spec.js
@@ -71,13 +71,13 @@ test.describe('Dummy Application', () => {
             page.locator('alert-list').locator('uq-alert').locator('[data-testid="alert-alert-2"]'),
         ).not.toBeVisible();
         await expect(
-            page.locator('alert-list').locator('uq-alert').locator('[data-testid="alert-action-desktop"]'),
+            page.locator('alert-list').locator('uq-alert').locator('[data-testid="alert-link"]'),
         ).toBeVisible();
+        await expect(page.locator('alert-list').locator('uq-alert').locator('[data-testid="alert-link"]')).toHaveText(
+            /Alert 1 button label/,
+        );
         await expect(
-            page.locator('alert-list').locator('uq-alert').locator('[data-testid="alert-action-desktop"]'),
-        ).toHaveText(/Alert 1 button label/);
-        await expect(
-            page.locator('alert-list').locator('uq-alert').locator('[data-testid="alert-action-desktop"]'),
+            page.locator('alert-list').locator('uq-alert').locator('[data-testid="alert-link"]'),
         ).toBeVisible();
 
         // not occluded by close button
