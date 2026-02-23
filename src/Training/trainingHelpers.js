e9edb7725e00da1c63a10782a917b1f8a067a1fd
Lea de Groot <l.degroot@library.uq.edu.au>
2026-02-23 10:34:18 +1000

    tweak: detect "is online" from the passed value, as well [YT AD-733] [YT AD-733]

--- src/Training/trainingHelpers.js
+++ src/Training/trainingHelpers.js
@@ -1,10 +1,8 @@
-export const isOnlineEventOnly = (event) =>
-    event?.venue?.startsWith('http') || (!!event?.isOnlineClass && !event.campus);
-export const isOnlineEvent = (event) => !!event?.isOnlineClass;
+export const isOnlineEvent = (event) => event?.venue?.startsWith('http') || !!event?.isOnlineClass;
 
 export const getVenueLabel = (event) => {
     let venue = event.venue;
-    if (isOnlineEventOnly(event)) {
+    if (event?.venue?.startsWith('http')) {
         // hide the url - we don't want them clicking straight through, we want them to go through studenthub
         venue = 'Online';
         if (event.venue.includes('zoom.us')) {
