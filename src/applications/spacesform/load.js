59c02b5cd8ee3d40bf811e91ce7e1a06db834030
Lea de Groot <l.degroot@library.uq.edu.au>
2026-04-21 15:22:43 +1000

    first cut of load js for spaces form [YT AD-792]

---
+++ src/applications/spacesform/load.js
@@ -0,0 +1,24 @@
+function ready(fn) {
+    if (document.readyState !== 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+function hasDebugParam(testHost) {
+    if (window.location.host !== testHost) {
+        return false;
+    }
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.has('debug') && urlParams.get('debug') === 'true';
+}
+
+function loadReusableComponentsAtom() {
+    console.log('spaces load file ready!');
+    if (hasDebugParam('testform')) {
+        console.log('has testform param');
+    }
+}
+
+ready(loadReusableComponentsAtom);
