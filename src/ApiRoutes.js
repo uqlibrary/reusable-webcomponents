6e5525d98233c99392db88b369f9169d3503029c
Lea de Groot <l.degroot@library.uq.edu.au>
2026-05-07 10:48:59 +1000

    undo: update the primo search api [YT AD-924]

--- src/ApiRoutes.js
+++ src/ApiRoutes.js
@@ -33,13 +33,15 @@ class ApiRoutes {
     }
 
     // Primo Suggestions API
-    // https://search.library.uq.edu.au/primaws/rest/pub/suggest?lang=en&q=cows&scope=61UQ_All&vid=61UQ_INST:61UQ
+    // https://primo-instant-apac.hosted.exlibrisgroup.com/solr/ac?q=cats&facet=off&fq=scope%3A()%2BAND%2Bcontext%3A(B)&rows=10&wt=json&json.wrf=byutv_jsonp_callback_c631f96adec14320b23f1cac342d30f6&_=2ef82775b72140a6bde04ea6e20012e4
     PRIMO_SUGGESTIONS_API_GENERIC = (keyword) => {
         return {
             apiUrl:
-                'https://search.library.uq.edu.au/primaws/rest/pub/suggest?lang=en&q=' +
+                'https://primo-instant-apac.hosted.exlibrisgroup.com/solr/ac?q=' +
                 keyword +
-                '&scope=61UQ_All&vid=61UQ_INST:61UQ' +
+                '&facet=off' +
+                '&fq=scope%3A()%2BAND%2Bcontext%3A(B)' +
+                '&rows=10' +
                 '&wt=json' +
                 '&json.wrf=byutv_jsonp_callback_c631f96adec14320b23f1cac342d30f6',
         };
